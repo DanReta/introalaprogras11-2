43638fbea3963304c47779098258ffe8c48aef3c
DanReta <91558759+DanReta@users.noreply.github.com>
2021-11-13 11:40:56 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,61 +1,119 @@
-package data;
+package com.company;
 import javax.swing.JOptionPane;
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        // [ 25 ,  ,   ,   ] 4 Posiciones
+        //   0   1   2   3 Indices
+        // [ , , , , ] 5 Posiciones
+        // Longitud 5 , 4
+        // [ , ] Indice 0
+        // [ 35, 40, 50 ] -> Numeros[0]
+        /*
 
-    }
+        //Me imprime la posicion 5
+        int[] numeros = new int[5];
+        numeros[0] = 35;
+        numeros[1] = 36;
+        numeros[2] = 37;
+        numeros[3] = 38;
+        numeros[4] = 39;
+        numeros[5] = 40;
 
-    static void ejercicioAutomovil()
-    {
-        // Instancia Clon de un Objeto
-        Automovil toyota = new Automovil();
-        toyota.motor = "2500";
-        toyota.modelo = "Land Cruiser";
-        toyota.puertas = 4;
-        toyota.pasajeros = 7;
 
-        System.out.println(toyota.motor);
-        System.out.println(toyota.modelo);
-        toyota.encender();
-        toyota.apagar();
-        boolean agua = toyota.tieneAgua();
-        if (agua) {
-            System.out.println("Tiene agua");
-        } else {
-            System.out.println("No tiene agua");
+
+        //Ejercicio 1
+        String[] nombre = new String[2];
+        nombre[0] = "Juan";
+        nombre[1] = "Luisa";
+
+        //Me va a recorrer el arreglo
+        // int es igul a 0 que es donde se empieza
+        //mientras int sea menor a la longitud del arreglo entonces
+        //ir incrementando
+
+        for (int i =0; i< nombre.length; i++){
+        //con esa i se comienza desde cero
+            System.out.println(nombre[i]); }
+            // 0 -> 35
+            // 1 -> 36
+            // 2 -> 37
+
+            //Ejercicio 2
+         float[] Valor= new float[3];
+         Valor[0] = 4f;
+         Valor[1] = 5f;
+         Valor[2] = 6f;
+         Valor[3] = 7f;
+         for (int i =0; i< Valor.length; i++){
+              System.out.println(Valor[i]);}
+
+        String[]  name = new String[2];
+        //pregunta
+        for (int i =0; i< name.length; i++){
+            name[i] = JOptionPane.showInputDialog("Ingrese el Nombre");
         }
+        //imprime
+        for (int i = 0; i<name.length; i++){
+            System.out.println(name[i]);
+        }
+
+        //Ejercicio 3
+        double[]  dato = new double[2];
+        for (int i =0; i< dato.length; i++){
+            double[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el numero"));
+        }
+        for (int i = 0; i<double.length; i++){
+            System.out.println(double[i]);
+        }
+
+        //Areglo de dos posiciones, en cada una hay 2 datos
+        //[ [] ,[] ] [0]
+        // [ [ [ ], 50 ], [75,100 ] ]
+        // [0][0][1] [0][1] [1][0] [1][1]
+
+        int[][] numeros2= new int[2][2];
+        //[ [,], [,] ]
+        numeros2[0][0]=25;
+        numeros2[0][1]=50;
+        numeros2[1][0]=75;
+        numeros2[1][1]=100;
+
+        for (int i =0 i<numeros2.length; i++){
+        // la longitud siempre va a hacer la misma, no va a cambiar "numeros2.lenght"
+             for (int j=0 j<numeros2.lenght; j++){
+             System.out.println(numeros2[i][j];
+             }
+         }
+         //Pide los numeros
+        Float[][] numerosFlotantes = new Float[2][2]
+        for (int i = 0; i < numerosFlotantes.length; i++) {
+            for (int j = 0; j < numerosFlotantes.length; j++) {
+                numerosFlotantes[i][j] = Float.parseFloat(JOptionPane.showInputDialog("Ingrese su numero"));
+                //
+            }
+        }
+        //Recorre los numeros
+        for (int i = 0; i < numerosFlotantes.length; i++) {
+            for (int j = 0; j < numerosFlotantes.length; j++) {
+                System.out.println(numerosFlotantes[i][j]);
+            }
+        }
+        */
+        double[][] numerosDouble = new double[5][5]
+        for (int i = 0; i < numerosDouble.length; i++) {
+            for (int j = 0; j < numerosDouble.length; j++) {
+                numerosDouble[i][j] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su numero"));
+                //
+            }
+        }
+        //Recorre los numeros
+        for (int i = 0; i < numerosDouble.length; i++) {
+            for (int j = 0; j < numerosDouble.length; j++) {
+                System.out.println(numerosDouble[i][j]);
+            }
+
+
     }
-
-    static void ejercicioHuesped()
-    {
-        Huespedes cliente = new Huespedes();
-        cliente.nombre = "Carlos Jimenez";
-        cliente.cedula = 115664724;
-        cliente.estadia = "Del 30 al 3 de noviembre";
-        cliente.habitacion = 345;
-
-        System.out.println(cliente.nombre);
-        System.out.println(cliente.habitacion);
-        cliente.solicitaComida();
-        cliente.salida();
-    }
-
-    static void ejercicioGrupos()
-    {
-        Grupos personas= new Grupos();
-        personas.nombre ="Alejandra Gonzales";
-        personas.horario= "De 11:00 am a 4:00pm ";
-        personas.salon=567;
-
-        System.out.println(personas.nombre);
-        System.out.println(personas.salon);
-        System.out.println(personas.horario);
-        personas.salonGrande();
-        personas.salonPequeño();
-
-    }
-
-
 }
+
